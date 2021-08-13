5aeb6072166ff173d720054783655a14cf33cc11
ikovic <kovic.ivan@gmail.com>
2021-08-13 12:23:21 +0200

    Initial project setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css');
+
+  // inline css
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
   return {
     dir: {
+      includes: "../_includes",
       input: 'pages',
-      output: 'static',
+      output: '_site',
     },
   };
 };
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Putting together some tooling designed to make working on smaller static sites easier. Nothing complex, for now I'm using eleventy to compile nunjacks templates to HTML.
 
-https://css-tricks.com/component-led-design-patterns-nunjucks-grunt/
-https://css-tricks.com/killer-features-of-nunjucks/
+## CSS
+CSS reset styles are inlined using [clean-css](https://www.11ty.dev/docs/quicktips/inline-css/).
